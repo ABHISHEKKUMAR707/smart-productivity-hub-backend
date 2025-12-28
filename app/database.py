85fa04adc67dfea2cd66595f0b7196f8d4b5a38f
Student ApnaCollege <ABHISHEKKUMAR70742@gmail.com>
2025-12-28 14:34:27 +0530

    first commit

---
+++ app/database.py
@@ -0,0 +1,8 @@
+
+"""
+TODO:
+- Configure database URL using environment variables
+- Create SQLAlchemy engine
+- Create session factory
+- Add get_db dependency for FastAPI
+"""
---
+++ app/main.py
@@ -0,0 +1,7 @@
+"""
+TODO:
+- Create FastAPI app instance
+- Add app metadata (title, version, description)
+- Add health check endpoint
+- Later: include routers
+"""
