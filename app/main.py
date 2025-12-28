0bfc4cff71f1019bbd5d309f6c12be252e355093
Anjana112-coder <anjanajha1109@gmail.com>
2025-12-28 18:24:03 +0530

    created two api

--- app/main.py
+++ app/main.py
@@ -5,3 +5,19 @@ TODO:
 - Add health check endpoint
 - Later: include routers
 """
+from fastapi import FastAPI
+
+app= FastAPI(
+    title = "smart-productivity-hub",
+      version= "0.128.0",
+        description = "backend-api-forTask"
+        )
+
+@app.get("/health")
+def health():
+    return {"health" : "healthy"} 
+
+@app.get("/")
+def devRoot():
+    return {"status": "ok", "message":"fastapi is running"}
+ 
