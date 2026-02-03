9732ce9fee5f0019fc322694e4d4b42c596b0051
Student ApnaCollege <ABHISHEKKUMAR70742@gmail.com>
2026-02-03 16:29:05 +0530

    TODO: USER.py

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+"""
+TODO:
+1. Create User table
+2. Fields:
+   - id
+   - email
+   - hashed_password
+   - role (user/admin)
+   - is_active
+   - created_at
+3. Use SQLAlchemy ORM
+4. Inherit from Base
+"""
