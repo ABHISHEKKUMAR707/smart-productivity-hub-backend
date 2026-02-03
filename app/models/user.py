753c450296e3de888ba39c597e19e66bc114dd4d
Anjana112-coder <anjanajha1109@gmail.com>
2026-02-03 18:38:19 +0530

    user table detail added

--- app/models/user.py
+++ app/models/user.py
@@ -11,3 +11,15 @@ TODO:
 3. Use SQLAlchemy ORM
 4. Inherit from Base
 """
+from sqlalchemy import Column, String, Integer, DateTime, Boolean
+from database import Base
+from pydantic import EmailStr
+
+class User(Base):
+    __tablename__='user'
+    id= Column(Integer, index=True, primary_key=True)
+    email: EmailStr =Column(String,unique= True, index=True, nullable=False)
+    hashed_password= Column(String, unique= True, nullable=False)
+    role= Column(String)
+    is_active= Column(Boolean)
+    created_at = Column(DateTime)
