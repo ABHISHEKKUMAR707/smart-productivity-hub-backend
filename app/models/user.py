38626dba5ebf886ab7a476b52f7f1f91e557ad1b
Student ApnaCollege <ABHISHEKKUMAR70742@gmail.com>
2026-02-05 12:12:57 +0530

    feat: add pydantic schemas for user create and read

--- app/models/user.py
+++ app/models/user.py
@@ -14,12 +14,21 @@ TODO:
 from sqlalchemy import Column, String, Integer, DateTime, Boolean
 from database import Base
 from pydantic import EmailStr
+from sqlalchemy import DateTime, func
+
 
 class User(Base):
     __tablename__='user'
     id= Column(Integer, index=True, primary_key=True)
     email: EmailStr =Column(String,unique= True, index=True, nullable=False)
     hashed_password= Column(String, unique= True, nullable=False)
-    role= Column(String)
-    is_active= Column(Boolean)
-    created_at = Column(DateTime)
+   #  role= Column(String)
+   #  is_active= Column(Boolean)
+    role = Column(String, default="user", nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(
+    DateTime(timezone=True),
+    server_default=func.now(),
+    index=True)
+
+    
---
+++ app/schemas/user.py
@@ -0,0 +1,7 @@
+"""
+TODO:
+1. UserCreate schema (for signup)
+2. UserRead schema (for API response)
+3. UserUpdate schema (future use)
+4. Never expose password or hashed_password in responses
+"""
