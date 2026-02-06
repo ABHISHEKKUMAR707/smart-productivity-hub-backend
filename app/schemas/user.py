0a44974a08f0c4d684e6712c9061dd6e0dc46b59
Anjana112-coder <anjanajha1109@gmail.com>
2026-02-06 19:36:48 +0530

    Changes made in schemas./user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,3 +5,23 @@ TODO:
 3. UserUpdate schema (future use)
 4. Never expose password or hashed_password in responses
 """
+from typing import Optional
+from pydantic import BaseModel , EmailStr
+
+class UserCreate(BaseModel):
+    userName:str
+    email: EmailStr
+    password:str
+
+
+class UserRead(BaseModel):
+    pass  
+
+class UserUpdate(BaseModel):
+    pass
+
+
+
+ 
+ 
+ 
